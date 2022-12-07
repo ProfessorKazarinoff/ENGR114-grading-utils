0e5a7b2d2df3639d3baec121934ae7a50a54c5f3
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2022-12-06 20:52:53 -0800

    worked on rename-file.py

--- rename-file.py
+++ rename-file.py
@@ -1,14 +1,15 @@
 # rename-file.py
 """
-A simple Python script to rename a files exported by the D2L LMS
+A simple Python script to copy and rename a files exported by the D2L LMS
 """
 
 from pathlib import Path
 
-old_f_str = "/Users/peter.kazarinoff/Downloads/Lab 1 Download Dec 6, 2022 455 PM/545474-744367 - first.last-Sep 29, 2022 342 PM-lab1.ipynb"
-old_f_path = Path(old_f_str)
+src_str = "test"
+src = Path(src_str)
 
-new_f_str = "/Users/peter.kazarinoff/Downloads/Lab 1 Download Dec 6, 2022 455 PM/first.last-file1.ipynb"
-new_f_path = old_f_path.with_name(new_f_str)
+dest_filename_str = src.stem.split(" - ")[1].split("-")[0] + "-" +  src.stem.split(" - ")[1].split("-")[2] + src.suffix
+dest = Path(src.parent, dest_filename_str)
 
-print(f"renamed: {str(old_f_path)} to: {str(new_f_path)}")
+with open(dest, "w"):
+    dest.write_text(src.read_text())
